fbc5244e8ea7a54f62fce11f9e367cdb5c47fb0d
Jin <wangatqueens@gmail.com>
2018-01-30 02:19:50 -0500

    initial structure

--- cfg/defaults.js
+++ cfg/defaults.js
@@ -37,6 +37,9 @@ function getDefaultModules() {
       test: /\.(png|jpg|gif|woff|woff2)$/,
       loader: 'url-loader?limit=8192'
     }, {
+      test: /\.json$/,
+      loader: 'json-loader'
+    }, {
       test: /\.(mp4|ogg|svg)$/,
       loader: 'file-loader'
     }]
