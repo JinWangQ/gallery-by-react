ceb1dce1bb7c258d562edee24ade37c89119db92
Jin <wangatqueens@gmail.com>
2018-01-30 14:08:14 -0500

    Style updates

--- src/components/Main.js
+++ src/components/Main.js
@@ -24,12 +24,12 @@ imagesData = (function getImageURL(imagesDataArray) {
 class ImgFigure extends React.Component {
 	render() {
 		return (
-			<figure>
+			<figure className="img-figure">
 				<img src={this.props.data.imageURL}
 					alt={this.props.data.title}
 				/>
 				<figcaption>
-					<h2>{this.props.data.title}</h2>
+					<h2 className="img-title">{this.props.data.title}</h2>
 				</figcaption>
 			</figure>
 		);
